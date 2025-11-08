7d0ec39d0b3a61c1ad782785c6d055439c317e12
BlochLior <blochlior@gmail.com>
2025-11-08 14:06:44 +0200

    Cheers done

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,30 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String upperCasedString = args[0].toUpperCase();
+                int n = Integer.parseInt(args[1]);
+                String swapToAn = "AEFHILMNORSX";
+                int i = 0;
+
+                // Give me a.. loop
+                while (i < upperCasedString.length()) {
+                        char current = upperCasedString.charAt(i);
+                        String outSpellString = "Give me ";
+                        if (swapToAn.indexOf(current) != -1) {
+                                outSpellString += "an ";
+                        } else {
+                                outSpellString += "a  ";
+                        }
+                        outSpellString += current + ": " + current + "!";
+                        System.out.println(outSpellString);
+                        i++;
+                }
+                System.out.println("What does that spell?");
+                int j = 0;
+                while (j < n) {
+                        System.out.println(upperCasedString + "!!!");
+                        j++;
+                }
+                
         }
 }
